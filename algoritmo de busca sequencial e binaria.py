e49ba386abe4bbb81fa0e67729dad648623657c2
cEnEdEsI <108566054+LorettaLetter@users.noreply.github.com>
2024-03-13 17:12:59 -0300

    algoritmo de busca sequencial e binaria.py

--- algoritmo de busca sequencial e binaria.py
+++ algoritmo de busca sequencial e binaria.py
@@ -43,21 +43,15 @@ def buscaSequencial(lista, chave):
     indice = 0
     for número in lista: # número in lista converte cada elemento da matriz para um número (nesse caso já é)
         if número == chave: # se o número achado é igual à chave, então achamos-o
-            return indice
+            return print("Posição da chave", chave, "na lista:", indice + 1)
         indice = indice + 1
-    return -1
+    return print("A chave", chave, "não se encontra na lista")
 
-chave = 24
 lista = [20, 5, 15, 24, 67, 45, 1, 76, 21, 11] # se a lista não estiver ordenada, use o quick_sort
+chave = 21
 quicksort(lista, 0, len(lista) - 1)
 print(lista)
-pos = buscaSequencial(lista, chave)
-
-if pos != -1:
-    print("Posição da chave", chave, "na lista:", pos + 1)
-else:
-    print("A chave", chave, "não se encontra na lista")
-# Posição da chave 45 na lista: 5
+buscaSequencial(lista, chave)
 
 
 '''------------------------------------------------------------------'''
@@ -80,7 +74,8 @@ posição seguinte a do meio até o final da lista. '''
 
 
 lista = [1, 5, 10, 13, 15, 18, 24, 38, 47, 54, 60]
-# observação: se a lista não tiver ordenada em ordem cresecente o comando lista.sort deve ser utilizado
+chave = 13
+# observação: se a lista não tiver ordenada o quick.sort deve sert utilizado
 
 def buscabinaria(lista,chave):
     pos_inic = 0
@@ -90,7 +85,7 @@ def buscabinaria(lista,chave):
         pos_mid = (pos_inic + pos_final)//2 # obs // divide nosso número e pega apenas a parte inteira
         
         if chave == lista[pos_mid]:
-            return pos_mid
+            return print ('Posição da chave ', chave, 'na lista:', pos_mid + 1)
         if chave < lista[pos_mid]:
             pos_final = pos_mid - 1  # -1 economiza processo
         if chave > lista[pos_mid]:
@@ -100,10 +95,9 @@ def buscabinaria(lista,chave):
 
 quicksort(lista, 0, len(lista) - 1)
 print(lista)
-buscabinaria(lista,14)
+buscabinaria(lista,chave)
         
-'''----------------------------------------------------------------------------'''
-''' Exercício prático 1: NAO FINALIZADO
+''' Exercício prático 1:
     Iremos refazer as funções de busca sequencial e busca binária
 assumindo que a lista possui chaves que podem ocorrer
 múltiplas vezes na lista. Neste caso, retornaremos uma
@@ -111,26 +105,22 @@ lista com todas as posições onde a chave foi encontrada. Se a
 chave não for encontrada na lista, retornaremos uma lista vazia.'''
 
 
-lista = [1, 5, 10, 13, 15, 13, 24, 38, 13, 54, 60]
+lista = [1, 5, 10, 13, 15, 13, 24, 38, 13, 54, 60, 13, 14, 13, 67, 832, 13]
 quicksort(lista, 0, len(lista) - 1)
 print(lista)
 chave = 13 # essa é nossa chave repetida
 
 def buscaSequencial(lista, chave): 
     indice = 0
-    indices = []
-    for número in lista:
-        if número != chave:
-            indice = indice + 1
-        elif número == chave:
-            indices.append(indice)
-            indice = indice + 1
-        else:
-            return print(indices)
-    return print("A chave",chave, "não se encontra na lista")
+    found = [] # aqui criamos a lista com os índices das posições da chave que procuramos
+    for número in lista: # número in lista converte cada elemento da matriz para um número (nesse caso já é)
+        if número == chave: # se o número achado é igual à chave, então achamos uma posição
+            found.append(indice + 1) # + 1 pois o indice começa em 0 e queremos nossa posição da forma correta
+        indice = indice + 1
+    if len(found) > 0:
+        return print("As posições da chave", chave, "na lista são:", found)
+    return print("A chave", chave, "não se encontra na lista") # essa linha só será executada o primeiro return não for executado, ou seja:
+# se nenhuma posição for achada para nossa chave, concluindo que ela não está na lista
 
-
-buscaSequencial(lista, 13)
-
-
+buscaSequencial(lista, chave)
 
